f87a5559e4c2a944223b10a648131aa8365dfc92
yiyujin <yujin94@gmail.com>
2024-10-21 13:03:25 -0400

    add descriptions

--- src/data2.js
+++ src/data2.js
@@ -2,15 +2,35 @@ const data2 = [
     {
         id : 0,
         title : "Fingers",
-        theme : `I want to isolate, or free apps from my smartphone. I feel like all these apps are imprisoned because their interactions are confined to the given screen (and guidelines from Google and Apple).
+        theme : `
+Final Output:
+Customized tactile controller for an app.
 
+I want to isolate, or free apps from my smartphone. I feel like all these apps are imprisoned because their interactions are confined to the given screen (and guidelines from Google and Apple).
 As a method of doing that, **I want to explore various finger compositions.** Thumb presses pretty much rule the current screen-based interaction. But we have 10 fingers!
+I also want to drive away from visual senses and discover ways for smooth interactions that don’t require people to look.
 
-I also want to drive away from visual senses and discover ways for smooth interactions that don’t require people to look.`,
+
+What's Next :
+I want to develop the project to mini pcomp projects.
+
+
+Disconnect to Connect :
+I bought this radio for over $200. I could just download a radio app on a phone for free, but I didn’t want to. I think an interaction includes pre and post stages too. What I mean by that in case of radio, is that all these stages matter: walking up to the radio, tuning into radio, listening to it, turning it off. This radio offered distinct analog interactions for each stage. If if it were on a phone, it would just be press-press-press.
+`,
         img : [
             "theme.png",
         ],
-        process : ``,
+        process : `I Learned / I Felt :
+        - Huge respect for hand made products.
+            - Craftsmanship
+            - Focus (Can't undo)
+            - Motivation is crucial
+        - Less fear of using tools, More need for using other tools.
+        - Quality matters.
+            - Detail
+            - Photographs
+        `,
         img2 : [
             "p1.jpg",
             "p2.jpg",
